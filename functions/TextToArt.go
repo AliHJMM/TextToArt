3b351016635be3d78bfa42438dd5adff1e28e4de
Ali <AliHasanJMM@gmail.com>
2024-08-29 03:21:04 +0300

    TextToArt Function

---
+++ functions/TextToArt.go
@@ -0,0 +1,37 @@
+package functions
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func TextToArt(args []string) {
+	txt := args[1]
+
+	textSlice := strings.Split(txt, "\\n")
+
+	if !charValidation(txt) {
+		fmt.Println("Error : invalid char")
+		os.Exit(1)
+	}
+	file, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println("Error : reading file")
+		os.Exit(1)
+	}
+	slice := strings.Split(string(file), "\n")
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ {
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					fmt.Print(slice[firstLine])
+				}
+				fmt.Println()
+			}
+		} else if j != len(textSlice)-1 {
+			fmt.Println("")
+		}
+	}
+}
