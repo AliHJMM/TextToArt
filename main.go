4226e37fbb33eef3eab4996f8b6dbd0b3391e933
Ali <AliHasanJMM@gmail.com>
2024-08-29 03:30:00 +0300

    Fixed program

--- main.go
+++ main.go
@@ -1,20 +1,138 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"TextToArt/functions"
+	"strings"
 )
 
 func main() {
 	args := os.Args 
 	if len(args) == 2 {
-		functions.TextToArt(args) 
+		TextToArt(args) 
 	} else if len(args) == 3 {
-		functions.TextToArtInTerminal(args) 
+		TextToArtInTerminal(args) 
 	} else if len(args) == 4 {
-		functions.TextToArtInFile(args)
+		TextToArtInFile(args) 
 	} else {
 		fmt.Println("Usage: go run . [STRING] [BANNER]\nEX: go run . something standard")
 	}
+}
+
+func TextToArt(args []string) {
+	txt := args[1]
+
+	textSlice := strings.Split(txt, "\\n")
+
+	if !charValidation(txt) {
+		fmt.Println("Error : invalid char")
+		os.Exit(1)
+	}
+
+	file, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println("Error : reading file")
+		os.Exit(1)
+	}
+	slice := strings.Split(string(file), "\n") 
+
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ { 
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					fmt.Print(slice[firstLine])
+				}
+				fmt.Println()
+			}
+		} else if j != len(textSlice)-1 {
+			fmt.Println("")
+		}
+	}
+}
+
+func TextToArtInFile(args []string) {
+	txt := args[2]
+	format := args[3]
+	str := ""
+
+	outputPtr := flag.String("output", "", "Output file name")
+	flag.Parse()
+
+	if *outputPtr == "" {
+		fmt.Println("Usage: go run . [STRING] [BANNER]\nEX: go run . something standard")
+		os.Exit(1)
+	}
+
+	textSlice := strings.Split(txt, "\\n")
+
+	if !charValidation(txt) {
+		fmt.Println("Error : invalid char")
+		os.Exit(1)
+	}
+
+	file, err := os.ReadFile(format + ".txt") 
+	if err != nil {
+		fmt.Println("Error : reading file")
+		os.Exit(1)
+	}
+	slice := strings.Split(string(file), "\n") 
+
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ {
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					str += slice[firstLine]
+				}
+				str += "\n"
+			}
+		} else if j != len(textSlice)-1 {
+			str += "\n"
+		}
+	}
+	os.WriteFile(*outputPtr, []byte(str), 0o644) 
+}
+
+func TextToArtInTerminal(args []string) {
+	txt := args[1]
+	format := args[2]
+	textSlice := strings.Split(txt, "\\n")
+
+	if !charValidation(txt) {
+		fmt.Println("Error : invalid char")
+		os.Exit(1)
+	}
+
+	file, err := os.ReadFile(format + ".txt") 
+	if err != nil {
+		fmt.Println("Error : reading file")
+		os.Exit(1)
+	}
+	slice := strings.Split(string(file), "\n") 
+
+	for j, txt := range textSlice {
+		if txt != "" {
+			for i := 0; i < 8; i++ {
+				for _, v := range txt {
+					firstLine := int(v-32)*9 + 1 + i
+					fmt.Print(slice[firstLine])
+				}
+				fmt.Println()
+			}
+		} else if j != len(textSlice)-1 {
+			fmt.Println("")
+		}
+	}
+}
+
+func charValidation(str string) bool {
+	slice := []rune(str)
+	for _, char := range slice {
+		if char < 32 || char > 126 {
+			return false
+		}
+	}
+	return true
 }
